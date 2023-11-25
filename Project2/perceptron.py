d59f7af80690a53d98213510ceaef5670381bcce
Simone Varriale s315962 <s315962@studenti.polito.it>
2023-11-25 13:47:32 +0100

    Started perceptron

--- Project2/perceptron.py
+++ Project2/perceptron.py
@@ -1,4 +1,4 @@
-from scipy.spatial import distance_matrix
+from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
 import numpy as np
 import time
@@ -7,21 +7,41 @@ import csv
 class Perceptron:
     
 
-    def __init__(self):
-        
-        # empty initialization of X and y
-        self.X = []
-        self.y = []
-        self.w = 0
-        
-    def train(self,X,y):
-        
-        self.X=X
+    def __init__(self, X, y):
+
+        '''
+        Initializes the parameters so that the have the same dimensions as the input data + 1
+        Inputs:
+        X - input data matrix of dimensions N x D => Reshapes the input data so that it can handle w_0, dim: N x (D+1)
+        y - assign the label of the training set
+        weights - model parameters initialized to zero size (D + 1) x 1
+        '''
+        self.X = PolynomialFeatures(1).fit_transform(X)
         self.y=y     
-       
-    def predict(self,X_new,p):
+        self.weights = np.zeros((X.shape[1]+1))
+        
+
+        
+    def train(self, alpha):
+
+        while True:
+            m = 0
+
+            for i in range (0, self.X.shape[0]):
+                if( np.dot(self.X[i,:], self.weights)*self.y[i] <= 0 ):
+                    self.weights = self.weights + (alpha)*self.X[i,:].T*self.y[i]
+                    m = m + 1
+            
+            if (m==0):
+                return
+
         
         
-        return 0    
+    def predict(self, X_test, y_test):
+        
+        X_test = PolynomialFeatures(1).fit_transform(X_test)
+        y_hat = np.sign(np.dot(X_test, self.weights))
+        
+        return y_hat
     
-    
+  
