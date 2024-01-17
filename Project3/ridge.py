df006bee473d75503d11f9d72704904fca2a77eb
Simone Varriale <93765397+simonevarriale@users.noreply.github.com>
2024-01-17 16:24:53 +0100

    Created folder for Project 3

---
+++ Project3/ridge.py
@@ -0,0 +1,30 @@
+from sklearn.preprocessing import PolynomialFeatures
+import pandas as pd
+import numpy as np
+
+
+class RidgeRegression:
+    
+
+    def __init__(self):
+
+        self.X = []
+        self.y = []
+        self.weights = []
+        
+
+        
+    def train(self, X, y):
+        return
+        
+
+        
+    def predict(self, X_test):
+
+        
+        
+        return 
+    
+        
+    
+  
